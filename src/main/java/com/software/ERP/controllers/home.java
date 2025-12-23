30d5197292c41b1132026260c82f02d1f699c45d
VISHESH <vishesh07007@gmail.com>
2025-12-23 22:13:07 +0530

    Update home.java

--- src/main/java/com/software/ERP/controllers/home.java
+++ src/main/java/com/software/ERP/controllers/home.java
@@ -1,4 +1,28 @@
 package com.software.ERP.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class home {
+
+    @GetMapping("/home")
+    public String home() {
+
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+
+        return "about";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        
+        return "contact_us";
+    }
+
+
 }
