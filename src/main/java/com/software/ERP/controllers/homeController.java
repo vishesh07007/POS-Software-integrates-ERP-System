69b49e03a313d0707324411a437e24231e2578be
VISHESH <vishesh07007@gmail.com>
2025-12-27 22:30:32 +0530

    changes on controller

--- src/main/java/com/software/ERP/controllers/homeController.java
+++ src/main/java/com/software/ERP/controllers/homeController.java
@@ -1,10 +1,11 @@
 package com.software.ERP.controllers;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class home {
+public class homeController {
 
     @GetMapping("/home")
     public String home() {
@@ -18,11 +19,24 @@ public class home {
         return "about";
     }
 
+    @GetMapping("/location")
+    public String location() {
+
+        return "Locate_us";
+    }
+
     @GetMapping("/contact")
     public String contact() {
 
         return "contact_us";
     }
 
+    @GetMapping("/login")
+    public String login(Model model) {
+
+        model.addAttribute("adminLogin",new AdminLogin());
+        return "login";
+
+    }
 
 }
---
+++ src/main/java/com/software/ERP/entities/Admin.java
@@ -0,0 +1,4 @@
+package com.software.ERP.entities;
+
+public class Admin {
+}
