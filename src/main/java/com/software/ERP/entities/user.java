7612b4901f9034ff8e777671eb3b72ae2b87b74d
VISHESH <vishesh07007@gmail.com>
2026-01-06 23:07:06 +0530

    Update user.java

--- src/main/java/com/software/ERP/entities/user.java
+++ src/main/java/com/software/ERP/entities/user.java
@@ -1,8 +1,40 @@
 package com.software.ERP.entities;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class user {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "phone", nullable = false, unique = true)
+    private String phone;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "address")
+    private String address;
+
+    public user(String name, String phone, String email, String address){
+        this.name=name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
 
 }
