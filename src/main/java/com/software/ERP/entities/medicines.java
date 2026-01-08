6a8c60ff5b13a0deaebdf6178d002e9c992e3534
VISHESH <vishesh07007@gmail.com>
2026-01-08 22:49:21 +0530

    Update medicines.java

--- src/main/java/com/software/ERP/entities/medicines.java
+++ src/main/java/com/software/ERP/entities/medicines.java
@@ -17,20 +17,16 @@ public class medicines {
     @Column(nullable = false)
     private String manufacturer;
 
-
+    private Double mrp;
 
 
     @Column(nullable = false)
-    private String genericName;  // e.g., "Paracetamol"
+    private String genericName;  // e.g, "Paracetamol"
 
     @Column(nullable = false, unique = true)
     private String code;  // Unique medicine code (like SKU)
 
-
-
-
-
-
-
+    @Column(columnDefinition = "boolean default true")
+    private boolean active = true;   // Instead of deleting a medicine: You deactivate it, and ensures new medicines are active by default
 
 }
