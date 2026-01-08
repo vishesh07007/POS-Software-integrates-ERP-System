2d995f6dfc37305f0b78f0e53f9d111b6da7d42d
VISHESH <vishesh07007@gmail.com>
2026-01-08 22:20:13 +0530

    Update medicines.java

--- src/main/java/com/software/ERP/entities/medicines.java
+++ src/main/java/com/software/ERP/entities/medicines.java
@@ -11,12 +11,22 @@ public class medicines {
     @Column(nullable = false)
     private String name;
 
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private String manufacturer;
+
+
+
 
     @Column(nullable = false)
     private String genericName;  // e.g., "Paracetamol"
 
     @Column(nullable = false, unique = true)
     private String code;  // Unique medicine code (like SKU)
+
+
 
 
 
