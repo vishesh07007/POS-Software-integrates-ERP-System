b83043f821306b848123f9536c196a437b577309
VISHESH <vishesh07007@gmail.com>
2026-02-19 22:47:30 +0530

    Create SaleItem.java

---
+++ src/main/java/com/software/ERP/entities/SaleItem.java
@@ -0,0 +1,35 @@
+package com.software.ERP.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "sale_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "medicine_id", nullable = false)
+    private Medicine medicine;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private Double sellingPrice;  // Price of one unit
+
+    private Double subtotal;  // quantity * sellingPrice
+
+    
+}
