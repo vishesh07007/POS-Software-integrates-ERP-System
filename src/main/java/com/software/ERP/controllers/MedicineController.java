d33fdd7e1236fc21761a933c77fc165deda1aa82
VISHESH <vishesh07007@gmail.com>
2026-02-22 23:53:27 +0530

    Update MedicineController.java

---
+++ src/main/java/com/software/ERP/controllers/MedicineController.java
@@ -0,0 +1,28 @@
+package com.software.ERP.controllers;
+
+import com.software.ERP.entities.Medicine;
+import com.software.ERP.repository.MedicineRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/medicines")
+
+public class MedicineController {
+
+    @Autowired
+    private MedicineRepo medicineRepository;
+
+    @GetMapping
+    public List<Medicine> getAllMedicines() {
+        return medicineRepository.findAll();
+    }
+
+
+
+}
